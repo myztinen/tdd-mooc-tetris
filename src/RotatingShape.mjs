3855163c06e7ae33874ef942b33f5cb07560e511
myztinen <myztinen@gmail.com>
2024-03-04 19:23:59 +0200

    Added input to two dimensional array

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,19 +1,37 @@
 export class RotatingShape {
     shape;
+    height;
+    width;
     constructor(shape) {
-        this.shape = shape.replace(/[\t\r\f ]+/g, '').concat('\n');
+        this.shape = this._stringToArray(shape);
+        this.height = this.shape.length;
+        this.width = this.shape[0].length;
       }
 
       toString() {
-        return this.shape
+        let boardString = "";
+        for (let row = 0; row < this.height; row++) {
+          for (let col = 0; col < this.width; col++) {
+              boardString  += this.shape[row] [col];
+          }
+          boardString += "\n";
+        }
+        return boardString;    
       }
 
       rotateRight() {
-        return;
+        return this;
       }
 
       rotateLeft() {
+        return this;
+      }
 
+      _stringToArray(input) {
+        const clearinput = input.replace(/[\t\r\f ]+/g, '');
+        const lines = clearinput.split('\n');
+        const twoDArray = lines.map(line => line.split(''));
+        return twoDArray;
       }
 
 }
