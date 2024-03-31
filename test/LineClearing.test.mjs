aa3605f9b557751abb9ab7e9ca8b89a3bc4bf23b
myztinen <myztinen@gmail.com>
2024-03-31 12:43:42 +0300

    Added more tests for clearing lines. level 8 done

--- test/LineClearing.test.mjs
+++ test/LineClearing.test.mjs
@@ -10,18 +10,11 @@ function fallToBottom(board) {
   }
 }
 
-describe("a Falling T tetromino", () => {
+describe("Board with two I tetrominoes", () => {
   let board;
   beforeEach(() => {
     board = new Board(10, 6);
-    board.drop(Tetromino.I_SHAPE);
-    board.moveRight();
-    board.moveRight();
-    board.moveRight();
-    fallToBottom(board);
-    board.drop(Tetromino.I_SHAPE);
-    board.moveLeft();
-    fallToBottom(board);
+    dropTwoHorizontalIBars(board);
   });
 
   test("line is cleared after a tick", () => {
@@ -53,11 +46,134 @@ describe("a Falling T tetromino", () => {
     );
 
   });
+
+  test("two lines are cleared after a tick", () => {
+    dropTwoHorizontalIBars(board);
+    dropOTetrominoeToLeftBottom(board);
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       OOIIIIIIII
+       OOIIIIIIII`
+    );
+    board.tick();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+
+  });
+});
+
+describe("Board with two T and one O tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+    dropTBars(board);
+    dropOTetrominoeToLeftBottom(board);
+    board.tick();
+  });
+  test("two lines are cleared after a tick", () => {
+    expect(board.toString()).to.equalShape(
+     `..........
+      ..........
+      ..........
+      ..........
+      OO..TTTTTT
+      OO...T..T.`
+    );
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    fallToBottom(board);
+    expect(board.toString()).to.equalShape(
+    `..........
+      ..........
+      ..........
+      ..........
+      ..........
+      OOOO.T..T.`
+    );
+  });
+});
+
+  describe("Board with four T and one O tetrominoes", () => {
+    let board;
+    beforeEach(() => {
+      board = new Board(10, 6);
+      dropTBars(board);
+      dropTBars(board);
+      dropOTetrominoeToLeftBottom(board);
+      board.tick();
+    });
+    test("two lines are cleared after a tick", () => {
+      expect(board.toString()).to.equalShape(
+       `..........
+        ..........
+        ....TTTTTT
+        .....T..T.
+        OO..TTTTTT
+        OO...T..T.`
+      );
+      board.drop(Tetromino.O_SHAPE);
+      board.moveLeft();
+      board.moveLeft();
+      fallToBottom(board);
+      expect(board.toString()).to.equalShape(
+       `..........
+        ..........
+        ..........
+        ....TTTTTT
+        .....T..T.
+        OOOO.T..T.`
+      );
+    });
 });
 
 
 
 
+function dropOTetrominoeToLeftBottom(board) {
+  board.drop(Tetromino.O_SHAPE);
+  board.moveLeft();
+  board.moveLeft();
+  board.moveLeft();
+  board.moveLeft();
+  board.tick();
+  board.tick();
+  board.tick();
+  board.tick();
+}
 
+function dropTwoHorizontalIBars(board) {
+  board.drop(Tetromino.I_SHAPE);
+  board.moveRight();
+  board.moveRight();
+  board.moveRight();
+  fallToBottom(board);
+  board.drop(Tetromino.I_SHAPE);
+  board.moveLeft();
+  fallToBottom(board);
+}
+
+function dropTBars(board) {
+  board.drop(Tetromino.T_SHAPE);
+  board.tick();
+  board.moveRight();
+  board.moveRight();
+  board.moveRight();
+  board.moveRight();
+  fallToBottom(board);
+  board.drop(Tetromino.T_SHAPE);
+  board.moveRight();
+  fallToBottom(board);
+}
 
 
